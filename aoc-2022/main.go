916920968e1c6f951dabd5ceb453ade031b24f0c
Sam Zanca <samuele@zanca.dev>
2023-07-19 02:06:38 -0400

    reading files in go

--- aoc-2022/main.go
+++ aoc-2022/main.go
@@ -1,7 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
+// func (s *string) leftPad(count int, char rune) {}
+// It appread that that ^, is not allowed. Rust can do that though :(
+
+// Writing this in Go because I want to learn Go
+func leftPad(str string, count int, char rune) string {
+	for len(str) < count {
+		str = string(char) + str
+	}
+	return str
 }
 
-// Sigh I gotta fix my dotfiles and then my path...
+func readInput(dayNum int) string {
+	dat, err := os.ReadFile("inputs/" + leftPad(strconv.Itoa(dayNum), 2, '0') + ".txt")
+	if err != nil {
+		panic(err)
+	}
+	return string(dat)
+}
+
+func main() {
+	// https://adventofcode.com/2022/day/1
+	input := readInput(1)
+	fmt.Println(input)
+	// Part 1
+
+	// Part 2
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Learning Golang
+
+I decided to make a monorepo and store all my fledgling golang projects/scripts so that I can have something to look back on.
